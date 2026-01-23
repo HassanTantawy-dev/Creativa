bc5e2c35e96eb54dace4a119015b86996247d8c5
HassanTantawy-dev <hasantantawy@icloud.com>
2026-01-23 18:10:39 +0200

    Add interactive to-do list functionality and navbar styling

---
+++ Dom/js/main.js
@@ -0,0 +1,19 @@
+// document.querySelector('#red').onclick=function(item) {
+//     document.querySelector('#demo').style.color='red'; 
+//     document.querySelector('#demo').style.textAlign=item.target.dataset.align;
+// }
+// document.querySelector('#green').onclick=function(item) {
+//     document.querySelector('#demo').style.color='green'; 
+//     document.querySelector('#demo').style.textAlign=item.target.dataset.align;
+// }
+// document.querySelector('#blue').onclick=function(item) {
+//     document.querySelector('#demo').style.color='blue'; 
+//     document.querySelector('#demo').style.textAlign=item.target.dataset.align;
+// }
+
+document.querySelectorAll('.pen').forEach(function(item) {
+    item.onclick=function() {
+        document.querySelector('#demo').style.color=item.dataset.color; 
+        document.querySelector('#demo').style.textAlign=item.dataset.align;
+    }
+});
---
+++ Dom_Task/js/main.js
@@ -0,0 +1,26 @@
+document.querySelector("#myform").addEventListener("submit", function (event) {
+    var works = document.querySelector("#works").value;
+    var result = document.querySelector("#result");
+    var btn = document.getElementById("btn");
+    
+    if (works == "") {
+        btn.setAttribute("class", "alert alert-danger text-center p-1 mt-2");
+        btn.innerHTML = "Please Enter Your Task";
+        event.preventDefault();
+    } else {
+        var myli = document.createElement("li");
+        myli.innerHTML = works;
+        result.appendChild(myli);
+        document.querySelector("#works").value = "";
+        event.preventDefault();
+    }
+});
+document.addEventListener("scroll", function () {
+    var navbar = document.querySelector(".navbar");
+    var scrollValue = window.scrollY;
+    if (scrollValue > 350) {
+        navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light bg-dark");
+    } else {
+        navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light bg-light");
+    }
+});
